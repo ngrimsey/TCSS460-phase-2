f607206c9cf34203f9ed8f0dd9ef0b7080e430f7
Dustin <103460016+DFeldt5@users.noreply.github.com>
2024-11-08 22:47:40 -0800

    Added delete routes for books

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, Router } from 'express';
-import { pool } from '../../core/utilities/sql_conn';
+import { pool, validationFunctions } from '../../core/utilities';
 import { IBook } from '../../core/models/booksModel';
+
+const { isNumberProvided } = validationFunctions;
 
 const bookRouter: Router = express.Router();
 
@@ -107,12 +109,17 @@ bookRouter.get('/author/:author', async (req: Request, res: Response) => {
         const result = await pool.query(query, [`%${author}%`]);
 
         if (result.rows.length === 0) {
-            return res.status(404).json({ message: 'No books found by this author' });
+            return res
+                .status(404)
+                .json({ message: 'No books found by this author' });
         }
 
         res.status(200).json(result.rows);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching books by author', error });
+        res.status(500).json({
+            message: 'Error fetching books by author',
+            error,
+        });
     }
 });
 
@@ -143,7 +150,10 @@ bookRouter.get('/title/:title', async (req: Request, res: Response) => {
         const result = await pool.query(query, [`%${title}%`]);
         res.status(200).json(result.rows);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching books by title', error });
+        res.status(500).json({
+            message: 'Error fetching books by title',
+            error,
+        });
     }
 });
 
@@ -172,7 +182,7 @@ bookRouter.get('/rating/:rating', async (req: Request, res: Response) => {
 
     if (exactRating < 1 || exactRating > 5) {
         return res.status(400).json({
-            message: 'Invalid rating - Rating must be between 1 and 5'
+            message: 'Invalid rating - Rating must be between 1 and 5',
         });
     }
 
@@ -181,7 +191,10 @@ bookRouter.get('/rating/:rating', async (req: Request, res: Response) => {
         const result = await pool.query(query, [exactRating]);
         res.status(200).json(result.rows);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching books by rating', error });
+        res.status(500).json({
+            message: 'Error fetching books by rating',
+            error,
+        });
     }
 });
 
@@ -218,8 +231,159 @@ bookRouter.get('/year/:year', async (req: Request, res: Response) => {
 
         res.status(200).json(result.rows);
     } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : "Unknown error occurred";
-        res.status(500).json({ message: 'Error fetching books by publication year', error: errorMessage });
+        const errorMessage =
+            error instanceof Error ? error.message : 'Unknown error occurred';
+        res.status(500).json({
+            message: 'Error fetching books by publication year',
+            error: errorMessage,
+        });
+    }
+});
+
+/**
+ * @api {delete} /closed/books/:isbn Delete Book by ISBN
+ * @apiName DeleteBookByISBN
+ * @apiGroup Books
+ *
+ * @apiParam {String} isbn ISBN of the book.
+ *
+ * @apiSuccess (Success 200) {String} message "Book deleted successfully"
+ * @apiSuccess (Success 200) {Object[]} book the entry objects of all deleted entries
+ *
+ * @apiError {Object} 404 Book not found.
+ *
+ */
+bookRouter.delete('/isbn/:isbn', async (req: Request, res: Response) => {
+    const { isbn } = req.params;
+
+    try {
+        const query = `DELETE FROM BOOKS WHERE isbn13 = $1 RETURNING *`;
+        const result = await pool.query(query, [isbn]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).send({
+            message: 'Book deleted successfully',
+            book: result.rows[0],
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching book', error });
+    }
+});
+
+/**
+ * @api {delete} /closed/books/author/:author Delete Books by Author
+ * @apiName DeleteBooksByAuthor
+ * @apiGroup Books
+ *
+ * @apiParam {String} author Author name to search for.
+ *
+ * @apiSuccess (Success 200) {String} message "Book(s) deleted successfully"
+ * @apiSuccess (Success 200) {Object[]} book the entry objects of all deleted entries
+ *
+ * @apiError {Object} 404 Book not found.
+ *
+ */
+bookRouter.delete('/author/:author', async (req: Request, res: Response) => {
+    const { author } = req.params;
+
+    try {
+        const query = `DELETE FROM BOOKS WHERE authors ILIKE $1 RETURNING *`;
+        const result = await pool.query(query, [`%${author}%`]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).send({
+            message: 'Book deleted successfully',
+            book: result.rows,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error fetching books by author',
+            error,
+        });
+    }
+});
+
+/**
+ * @api {delete} /closed/books/title/:title Delete Books by Title
+ * @apiName DeleteBooksByTitle
+ * @apiGroup Books
+ *
+ * @apiParam {String} title Title to search for.
+ *
+ * @apiSuccess (Success 200) {String} message "Book(s) deleted successfully"
+ * @apiSuccess (Success 200) {Object[]} book the entry objects of all deleted entries
+ *
+ * @apiError {Object} 404 Book not found.
+ *
+ */
+bookRouter.delete('/title/:title', async (req: Request, res: Response) => {
+    const { title } = req.params;
+
+    try {
+        const query = `DELETE FROM BOOKS WHERE title ILIKE $1 RETURNING *`;
+        const result = await pool.query(query, [`%${title}%`]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).send({
+            message: 'Book deleted successfully',
+            book: result.rows,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error fetching books by title',
+            error,
+        });
+    }
+});
+
+/**
+ * @api {delete} /closed/books/cursor Request to delete entries by cursor
+ *
+ * @apiDescription Request to delete a range of entries using a cursor.
+ *
+ * @apiName DeleteBooksByCursor
+ * @apiGroup Books
+ *
+ * @apiQuery {number} limit the number of entry objects to delete. Note, if a value less than
+ * 0 is provided or a non-numeric value is provided or no value is provided, the default limit
+ * amount of 10 will be used.
+ *
+ * @apiQuery {number} cursor the value used in the lookup of entry objects to delete. When no cursor is
+ * provided, the result is the first set of paginated entries.  Note, if a value less than 0 is provided
+ * or a non-numeric value is provided results will be the same as not providing a cursor.
+ *
+ * @apiSuccess (Success 200) {String} message "Book(s) deleted successfully"
+ * @apiSuccess (Success 200) {Object[]} book the entry objects of all deleted entries
+ *
+ */
+bookRouter.delete('/cursor', async (request: Request, response: Response) => {
+    const theQuery = `DELETE FROM Books
+                        WHERE bookid > $2  
+                        ORDER BY bookid
+                        LIMIT $1
+                        RETURNING *;`;
+
+    const limit: number =
+        isNumberProvided(request.query.limit) && +request.query.limit > 0
+            ? +request.query.limit
+            : 10;
+    const cursor: number =
+        isNumberProvided(request.query.cursor) && +request.query.cursor >= 0
+            ? +request.query.cursor
+            : 0;
+
+    const values = [limit, cursor];
+    try {
+        const result = await pool.query(theQuery, values);
+        response.status(200).send({
+            message: 'Book deleted successfully',
+            book: result.rows,
+        });
+    } catch {
+        response.status(500).json({ message: 'Error deleting books' });
     }
 });
 
